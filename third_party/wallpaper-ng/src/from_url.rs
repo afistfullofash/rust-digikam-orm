9d30e52ef04def0519264fe7d6735878216858eb
Natalie <natalie.atkinson95@pm.me>
2026-03-12 17:04:43 +1100

    Patch wallpaper-ng for Windows enquote dependency

---
+++ third_party/wallpaper-ng/src/from_url.rs
@@ -0,0 +1,12 @@
+use crate::{Error, Result};
+use std::fs::File;
+
+pub fn download_image(url: &str) -> Result<String> {
+    let cache_dir = dirs::cache_dir().ok_or(Error::NoConfigDir)?;
+    let file_path = cache_dir.join("wallpaper.jpg");
+
+    let mut file = File::create(&file_path)?;
+    reqwest::blocking::get(url)?.copy_to(&mut file)?;
+
+    Ok(file_path.to_str().to_owned().ok_or(Error::InvalidPath)?.into())
+}
