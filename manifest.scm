5af2051bbe53833fe9c9dee671e9629b919607cd
Natalie Atkinson <natalie.atkinson95@pm.me>
2026-03-13 14:35:38 +1100

    Increased Version to 0.3.0

--- manifest.scm
+++ manifest.scm
@@ -1,8 +1,10 @@
 (use-modules (gnu packages rust)
+	     (gnu packages sqlite)
 	     (afistfullofash packages package-management))
 
 (manifest
  (list
+  (package->manifest-entry sqlite)
   (package->manifest-entry rust)
   (package->manifest-entry rust "tools")
   (package->manifest-entry rust "cargo")
